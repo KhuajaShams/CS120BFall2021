474620204c81e0fc4d12f1e7cb1d32236c2ac58c
KhuajaShams <ksham004@ucr.edu>
2021-10-09 01:45:01 -0700

    updated lab3 part 3

---
+++ Lab3_bitManipulation/turnin/ksham004_lab3_part3.c
@@ -0,0 +1,53 @@
+/*	Author: Khuaja Shams
+ *  Partner(s) Name: 
+ *	Lab Section: 21
+ *	Assignment: Lab #3  Exercise #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; 
+	PORTA = 0x00;
+	DDRB = 0xFF; 
+	PORTB = 0x00;
+
+    /* Insert your solution below */
+    while (1) {
+	unsigned char tmpA, tmpA2, tmpB;
+	tmpA = PINA & 0x0F;
+	tmpA2 = PINA & 0x70;
+	tmpB = PINB;
+	if (tmpA <= 0)
+		tmpB = 0x00;
+	else if (tmpA <= 2) 
+		tmpB = 0x20;
+	else if (tmpA <= 4)
+		tmpB = 0x30;
+	else if (tmpA <= 6) 
+		tmpB = 0x38;
+	else if (tmpA <= 9)
+		tmpB = 0x3C;
+	else if (tmpA <= 12)
+		tmpB = 0x3E;
+	else if (tmpA <= 15)
+		tmpB = 0x3F;
+	else 
+		tmpB = 0x00;
+
+	if (tmpA <= 4)
+		tmpB = tmpB | 0x40; 
+	if (tmpA2 == 0x30)
+		tmpB = tmpB | 0x80;
+		
+	PORTB = tmpB;
+    }
+    return 1;
+}
