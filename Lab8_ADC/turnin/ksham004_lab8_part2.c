206cc3ef2ccac764bb18360a4b6db8ab4d08bfe8
KhuajaShams <ksham004@ucr.edu>
2021-10-28 19:18:08 -0700

    lab 8 done

--- Lab8_ADC/turnin/ksham004_lab8_part2.c
+++ Lab8_ADC/turnin/ksham004_lab8_part2.c
@@ -1,12 +1,12 @@
 /*	Author: Khuaja Shams
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab # 8 Exercise # 3
+ *	Assignment: Lab # 8 Exercise # 2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
- *	Demo Link: https://www.youtube.com/watch?v=t8VdTKRZQdY 
+ *	Demo Link: https://www.youtube.com/watch?v=NmwRdWSNqBE 
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
@@ -19,19 +19,17 @@ void ADC_init() {
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+	DDRB = 0xFF; PORTB = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
-	unsigned char tmpB = 0x00;
-	unsigned short MAX = 0x138;
+	unsigned char tmpB, tmpD = 0x00;
 	ADC_init();
     /* Insert your solution below */
     while (1) {
 	unsigned short my_short = ADC;
-	if (my_short >= MAX/2)
-		tmpB = 0x01;
-	else 
-		tmpB = 0x00;
-
-	PORTD = tmpB;
+	tmpB = (char) my_short;
+	tmpD = (char) (my_short >> 8);
+	PORTB = tmpB;
+	PORTD = tmpD;
     }
     return 1;
 }
